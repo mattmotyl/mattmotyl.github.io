50922e0f31c4ae6b74a06b16fc9fc967095bd8e3
JP Prims <jprim@PSCH-motyl-L2.ad.uic.edu>
2018-09-26 16:41:21 -0500

    Changed the name of the anesclean function to makenumeric. Now works with non-ANES data. Allows you to enter unique NA strings, and to tell the function if it's ANES data or not.
    na.strings = NULL by default.
    anes = FALSE by default.

--- SICUtils.R
+++ SICUtils.R
@@ -19,12 +19,23 @@
     # Cleaning ANES data. You'll still need to make sure that they're coded in a way that makes sense after running this. 
       # (ANES coding varies, so we can't automate making sure that the numbers make sense.)
     
-    anesclean <- function(colsn){
+    makenumeric <- function(colsn, anes = FALSE, na.strings = NULL){
       # "x" is the column (or columns) of the variables that you'd like to clean.
-      colsn.r <- lapply(colsn, function(x) replace(x, grep("[-]", x), NA))
-      colsn.d <- as.data.frame(lapply(colsn.r, function(x) factor(x)))
-      colnum <- ncol(colsn)
-      colsn.d[,1:colnum] <- sapply(colsn.d[,1:colnum], as.numeric)
+      # This checks if it's ANES
+      ifelse(anes == FALSE, 
+             ifelse(is.null(na.strings), # Did they enter NA strings? 
+                    colsn.r <- as.data.frame(colsn),
+                    colsn.r <- lapply(as.data.frame(colsn), function(x) gsub(na.strings, NA, x)) # If it's not ANES data
+             ),
+             colsn.r <- lapply(as.data.frame(colsn), function(x) replace(x, grep("[-]", x), NA)) # If it is ANES data
+      )
+      colsn.r <- lapply(colsn.r, function(x) gsub("[^0-9\\.]", "", x)) # Removes any alphabetic characters
+      colsn.d <- as.data.frame(colsn.r)
+      colnum <- ncol(as.data.frame(colsn))
+      ifelse(colnum == 1,
+             return("Error: Please enter at least two columns."),
+             colsn.d[,1:colnum] <- sapply(colsn.d[,1:colnum], as.numeric)
+      )
       # Adding an indication that these are numeric. 
       colnames(colsn.d) <- paste(colnames(colsn.d), "n", sep = ".")
       colsn.d
